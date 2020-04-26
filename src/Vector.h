0d1ed3881993563cf7d1953ba59e035b56ce2bb4
laujinseoi <laujinseoi@yeah.net>
2020-04-27 00:33:21 +0800

    [*]Add some basics function @ Vector.h

--- src/Vector.h
+++ src/Vector.h
@@ -9,22 +9,61 @@ template <class T>
 class Vector
 {
    protected:
-    Rank size_;
-    int  capacity_;
-    T*   elem_;
+    Rank size_     = 0;
+    int  capacity_ = 0;
+    T*   elem_     = nullptr;
 
-    void copyFrom(T const* A, Rank lo, Rank hi);  // 复制数组区间A[lo, hi)
-    void expand();                                // 空间不足时扩容
-    void shrink();                                // 装填因子过小时压缩
-    bool bubble(Rank lo, Rank hi);                // 扫描交换
-    void bubbleSort(Rank lo, Rank hi);            // 气泡排序算法
-    Rank max(Rank lo, Rank hi);                   // 选取最大元素
-    void selectSorted(Rank lo, Rank hi);          // 选择排序算法
-    void merge(Rank lo, Rank hi);                 // 归并算法
-    void mergeSort(Rank lo, Rank hi);             // 归并排序算法
-    Rank partition(Rank lo, Rank hi);             // 轴点构造算法
-    void quickSort(Rank lo, Rank hi);             // 快速排序算法
-    void heapSort(Rank lo, Rank hi);              // 堆排序
+    // 复制数组区间A[lo, hi)
+    void copyFrom(T const* A, Rank lo, Rank hi)
+    {
+        if (elem_ != nullptr)
+            delete[] elem_;
+        elem_ = new T[capacity_ = 2 * (hi - lo)];
+        size_ = 0;
+        while (lo < hi)
+            elem_[size_++] = A[lo++];
+    }
+
+    // 空间不足时扩容
+    void expand()
+    {
+        if (size_ < capacity_)
+            return;
+
+        if (capacity_ < DEFAULT_CAPACITY)
+            capacity_ = DEFAULT_CAPACITY;
+
+        T* old_elem = elem_;
+        elem_       = new T[capacity_ << 1];
+        for (int i = 0; i < size_; i++)
+            elem_[i] = old_elem[i];
+
+        delete[] old_elem;
+    }
+
+    // 装填因子过小时压缩
+    void shrink()
+    {
+        if (capacity_ == size_)
+            return;
+
+        T* old_elem = elem_;
+        elem_       = new T[capacity_ = size_];
+        for (int i = 0; i < size_; i++)
+            elem_[i] = old_elem[i];
+
+        delete[] old_elem;
+    }
+
+    bool bubble(Rank lo, Rank hi);        // 扫描交换
+    void bubbleSort(Rank lo, Rank hi);    // 气泡排序算法
+    Rank max(Rank lo, Rank hi);           // 选取最大元素
+    void selectSorted(Rank lo, Rank hi);  // 选择排序算法
+    void merge(Rank lo, Rank hi);         // 归并算法
+    void mergeSort(Rank lo, Rank hi);     // 归并排序算法
+    Rank partition(Rank lo, Rank hi);     // 轴点构造算法
+    void quickSort(Rank lo, Rank hi);     // 快速排序算法
+    void heapSort(Rank lo, Rank hi);      // 堆排序
 
    public:
     Vector(int c = DEFAULT_CAPACITY, int s = 0, T v = 0)
@@ -41,11 +80,15 @@ class Vector
         copyFrom(V.begin(), lo, hi);
     }
 
-    ~Vector() { delete[] elem_; }
+    ~Vector()
+    {
+        if (elem_ != nullptr)
+            delete[] elem_;
+    }
 
     // 只读接口
     T    front() const { return elem_[0]; }
-    T    get(int r) const;        // 获取秩为r的元素
+    T    get(int r) const;               // 获取秩为r的元素
     int  size() const { return size_; }  // 返回元素总数
     bool empty() const { return !size_; }
     int disordered() const;  // 判断所有元素是否已按照非降序排列
@@ -61,22 +104,29 @@ class Vector
 
     // 可写接口
     T&         operator[](Rank r) const;
-    Vector<T>& operator=(Vector<T> const&);
-    T*   begin() const { return &elem_[0]; }
+    Vector<T>& operator=(const Vector<T>& V)
+    {
+        if (elem_ != nullptr)
+            delete[] elem_;
+        copyFrom(V.begin(), 0, V.size());
+        return *this;  // 方便链式复制
+    }
+
+    T*   begin() const { return elem_; }
     void put(int r, const T& e);  //用e替换秩为r元素的数值
 
     T remove(Rank r);  //移除秩为r的元素，返回元素中原存放的对象
     int remove(Rank lo, Rank hi);
 
     Rank insert(Rank r, const T& e);  // e作为秩为r元素插入，原后继元素依次后移
-    Rank insert(const T& e) { return insert(size_, e); } // 插入元素到最后
+    Rank insert(const T& e) { return insert(size_, e); }  // 插入元素到最后
 
     void sort(Rank lo, Rank hi);         // 局部排序
     void sort() { sort(0, size_); }      // 整体排序
     void unsort(Rank lo, Rank hi);       // 局部置乱
     void unsort() { unsort(0, size_); }  // 整体置乱
-    void deduplicate();  // 无序去重
-    void uniquify();     // 有序去重
+    void deduplicate();                  // 无序去重
+    void uniquify();                     // 有序去重
 
     // 遍历
     void traverse(void (*)(T&));
